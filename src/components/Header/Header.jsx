0b116536204c45a58c4782b72dfab0861e86862d
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-17 18:04:17 +0600

    fixed header button style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <header className="py-2 px-3 bg-primary text-white d-flex flex-column flex-lg-row align-items-lg-center">
-      <div className="d-flex">
+      <div className="d-flex align-items-center">
         <div className="fs-2">Business Consultant</div>
 
         <button
