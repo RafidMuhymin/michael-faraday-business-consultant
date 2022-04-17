3bf7ff64617307467e0ae667ff5fd64b58fffbec
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-17 19:09:02 +0600

    added pacifico font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className="py-2 px-3 bg-primary text-white d-flex flex-column flex-lg-row align-items-lg-center">
       <div className="d-flex align-items-center">
-        <div className="fs-2">Business Consultant</div>
+        <div className="fs-2 brand-heading">Business Consultant</div>
 
         <button
           onClick={handleClick}
