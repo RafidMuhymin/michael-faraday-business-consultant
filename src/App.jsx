954599f3b61112476f696791b609cba361453edf
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-17 16:46:48 +0600

    setup necessary things

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Checkout from './components/Checkout/Checkout';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </div>
   );
 }
