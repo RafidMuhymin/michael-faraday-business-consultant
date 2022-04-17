ffad3a177e9929154a64bb8be7f5b32b7247491f
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-17 18:02:33 +0600

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import "./Footer.css"
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="pt-2 px-3 text-center gap-3 bg-dark d-flex flex-column align-items-center">
+      <nav className="d-flex flex-wrap justify-content-center text-center gap-3">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/checkout">Checkout</Link>
+      </nav>
+
+      <p className="text-white">
+        Copyright &copy; {new Date().getFullYear()}{" "} Business Consultant
+      </p>
+    </footer>
+  );
 }
