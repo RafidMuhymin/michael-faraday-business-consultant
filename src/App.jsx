121b4171ad6f0c6a1580b0cdf6f51c70065d6b91
RafidMuhymin <rafidmuhymin@gmail.com>
2022-04-17 23:34:56 +0600

    wrapped every page with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
-import About from './components/About/About';
-import Blog from './components/Blog/Blog';
-import Checkout from './components/Checkout/Checkout';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import SignUp from './components/SignUp/SignUp';
+import "./App.css";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Checkout from "./components/Checkout/Checkout";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
@@ -20,6 +23,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
